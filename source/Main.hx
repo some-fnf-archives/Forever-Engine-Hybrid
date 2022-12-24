b07ab65e8e0432734809502e94160e2278c83cb1
Yoshubs <37424048+Yoshubs@users.noreply.github.com>
2022-12-24 02:46:55 -0500

    quick fix

--- source/Main.hx
+++ source/Main.hx
@@ -111,7 +111,7 @@ class Main extends Sprite
 
 		// here we set up the base game
 		var gameCreate:FlxGame;
-		gameCreate = new FlxGame(gameWidth, gameHeight, mainClassState, #if (fixel < "5.0.0") zoom, #end framerate, framerate, skipSplash);
+		gameCreate = new FlxGame(gameWidth, gameHeight, mainClassState, #if (flixel < "5.0.0") zoom, #end framerate, framerate, skipSplash);
 		addChild(gameCreate); // and create it afterwards
 
 		// begin the discord rich presence
